9d32efd03bb4477f5b2c61bd3d07bbc10bd40876
Siajiza <80329210+siajiza@users.noreply.github.com>
2021-04-15 08:07:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,15 @@ angels = teams[1].get('a', 'Team not found')
 
 print(angels)
 
+sales = {
+    'google': 20,
+    'facebook': 42,
+    'twitter': 10,
+    'offline': 12,
+}
+
+print('g ' + sales['google'] * '$')
+print('f ' + sales['facebook'] * '$')
+print('t ' + sales['twitter'] * '$')
+print('o ' + sales['offline'] * '$')
+
